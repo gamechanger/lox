a8cc93ad4f1e04ff5e2628ecf088762a794225ec
Travis Thieman <travis.thieman@gmail.com>
2014-07-02 14:51:31 -0400

    Write way nicer tests with supertest

--- test/lox_test.js
+++ test/lox_test.js
@@ -1,6 +1,6 @@
 require('should');
 
-var request = require('request');
+var request = require('supertest');
 var uuid = require('node-uuid');
 
 var app = require('../lox');
@@ -23,59 +23,49 @@ describe("The HTTP endpoint", function() {
   });
 
   describe("POST /lock", function() {
-    var url = 'http://localhost:' + config.port + '/lock';
 
     it("returns 400 without all required parameters", function(done) {
-      request.post({url: url}, function(err, res, body) {
-        res.statusCode.should.be.eql(400);
-        done(err);
-      });
+      request(app).post('/lock').expect(400, done);
     });
 
     it("returns 201 and lockId on acquiring a fresh lock", function(done) {
-      var form = {key: testKey, concurrentKeys: 1, ttlSeconds: 60};
-      request.post({url: url, json: form}, function(err, res, body) {
-        res.statusCode.should.be.eql(201);
-        body.lockId.should.be.ok;
-        done(err);
-      });
+      request(app).post('/lock')
+        .send({key: testKey, concurrentKeys: 1, ttlSeconds: 60})
+        .expect(201)
+        .expect(function(res) {
+          res.body.lockId.should.be.ok;
+        })
+        .end(done);
     });
 
     it("returns 204 when failing to acquire a lock", function(done) {
       var form = {key: testKey, concurrentKeys: 1, ttlSeconds: 60};
-      request.post({url: url, json: form}, function(err, res, body) {
-        request.post({url: url, json: form}, function(err, res, body) {
-          res.statusCode.should.be.eql(204);
-          (body === undefined).should.be.true;
-          done(err);
-        });
+      request(app).post('/lock').send(form).end(function(err, res) {
+        if (err) { done(err); }
+        request(app).post('/lock')
+          .send(form)
+          .expect(204)
+          .expect(function(res) {
+            res.body.should.be.eql({});
+          })
+          .end(done);
       });
     });
-
   });
 
   describe("DELETE /lock/:lockId", function() {
-    var url = 'http://localhost:' + config.port + '/lock';
-    var fullUrl = url + '/exampleLockId';
 
     it("returns 204 on non-existent lock", function(done) {
-      request.del({url: fullUrl}, function(err, res, body) {
-        if (err) { done(err); }
-        res.statusCode.should.be.eql(204);
-        done(err);
-      });
+      request(app).del('/lock/' + testKey)
+        .expect(204, done);
     });
 
     it("returns 204 on an existing lock", function(done) {
       var form = {key: testKey, concurrentKeys: 1, ttlSeconds: 60};
-      request.post({url: url, json: form}, function(err, res, body) {
+      request(app).post('/lock').send(form).end(function(err, res) {
         if (err) { done(err); }
-        var lockId = body.lockId;
-        request.del({url: url + '/' + lockId}, function(err, res, body) {
-          if (err) { done(err); }
-          res.statusCode.should.be.eql(204);
-          done(err);
-        });
+        request(app).del('/lock/' + res.body.lockId)
+          .expect(204, done);
       });
     });
 
